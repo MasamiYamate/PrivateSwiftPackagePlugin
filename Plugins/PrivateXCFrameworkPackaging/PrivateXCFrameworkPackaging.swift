506fc06b73a067f4eeb6c08db7932c76e955680f
Masami Yamate <m.yamate@mail.yamatte.net>
2023-10-07 11:36:36 +0900

    Dir把握のためのTest Pluginの追加

---
+++ Plugins/PrivateXCFrameworkPackaging/PrivateXCFrameworkPackaging.swift
@@ -0,0 +1,25 @@
+//
+//  PrivateXCFrameworkPackaging.swift
+//
+//
+//  Created by Masami on 2023/10/07.
+//
+
+import PackagePlugin
+
+@main
+struct PrivateXCFrameworkPackaging: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        return [
+            .prebuildCommand(
+                displayName: "Run generate keys",
+                executable: Path("/usr/bin/make"),
+                arguments: [
+                    "test"
+                ],
+                outputFilesDirectory: context.pluginWorkDirectory
+            )
+        ]
+    }
+}
+
--- Sources/PrivateXCFrameworkPackaging/PrivateSwiftPackagePlugin.swift
+++
@@ -1,6 +0,0 @@
-public struct PrivateXCFrameworkPackaging {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
