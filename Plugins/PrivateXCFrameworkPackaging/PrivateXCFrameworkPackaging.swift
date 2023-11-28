f6bfd1967caaa8f90dd0c3b5e9b1fb0908b8523d
Masami Yamate <m.yamate@mail.yamatte.net>
2023-11-28 23:10:49 +0900

    wip

--- Plugins/PrivateXCFrameworkPackaging/PrivateXCFrameworkPackaging.swift
+++ Plugins/PrivateXCFrameworkPackaging/PrivateXCFrameworkPackaging.swift
@@ -14,15 +14,11 @@ struct PrivateXCFrameworkPackaging: CommandPlugin {
         context: PluginContext,
         arguments: [String]
     ) async throws {
-        print(context)
-        print(arguments)
-        print("hoge!!")
-        print(context.package.directory)
-        print(context.pluginWorkDirectory)
-        print(makeCheckoutProjectPath(context: context))
+        let checkoutProjectPath = makeCheckoutProjectPath(context: context)!
         let process = Process()
-        process.currentDirectoryURL = URL(fileURLWithPath: context.package.directory.string)
+        process.currentDirectoryURL = URL(fileURLWithPath: checkoutProjectPath)
         process.executableURL = URL(fileURLWithPath: "/usr/bin/make")
+        process.arguments = ["binary-release", "\(checkoutProjectPath)"]
         process.launch()
         process.waitUntilExit()
     }
@@ -30,7 +26,7 @@ struct PrivateXCFrameworkPackaging: CommandPlugin {
 
 private extension PrivateXCFrameworkPackaging {
     
-    func makeCheckoutProjectPath(context: PluginContext) {
+    func makeCheckoutProjectPath(context: PluginContext) -> String? {
         let pluginWorkDirectoryPath = URL(string: context.pluginWorkDirectory.string)
         let sourcePackagesDirectoryPath = pluginWorkDirectoryPath?
             .deletingLastPathComponent()
@@ -38,7 +34,7 @@ private extension PrivateXCFrameworkPackaging {
         let checkoutDirectoryPath = sourcePackagesDirectoryPath?
             .appending(component: "checkouts")
             .appending(component: "PrivateXCFrameworkPackaging")
-        print(checkoutDirectoryPath?.absoluteString ?? "")
+        return checkoutDirectoryPath?.absoluteString
     }
     
 }
